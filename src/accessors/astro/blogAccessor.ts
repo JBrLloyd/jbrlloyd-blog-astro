25fbb20e87710731b00b2a4ca6816b82d55c69ff
Jackson Lloyd <jackson@jbrlloyd.dev>
2024-07-12 01:37:10 +1000

    next and previous blog post

--- src/accessors/astro/blogAccessor.ts
+++ src/accessors/astro/blogAccessor.ts
@@ -1,14 +1,36 @@
-import { getCollection } from 'astro:content';
+import { getCollection, type CollectionEntry } from 'astro:content';
 const isProd = import.meta.env.PROD;
 
-export const getBlogPosts = async () => {
-  const posts = await getCollection('blog')
-  return posts
-    .filter(p =>
-      // Only apply filtering of posts to production app
-      !isProd
-      || (!p.data.draft && p.slug !== 'markdown-style-guide' && p.slug !== 'using-mdx'))
+type BlogPostData = CollectionEntry<'blog'>;
+
+export const getAllBlogPosts = async () => {
+  return  await getCollection('blog', (p) => {
+    // Only apply filtering of posts to production app
+    return !isProd
+      || (!p.data.draft && p.slug !== 'markdown-style-guide' && p.slug !== 'using-mdx')
+  })
+}
+
+export const sortPostsByPublishDesc = (blogPosts: CollectionEntry<'blog'>[]) => {
+  return blogPosts
     .sort((a, b) =>
       b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
     );
+}
+
+export type SortedBlogPost = BlogPostData & {
+  previousPostSlug: string | null
+  nextPostSlug: string | null
+}
+
+export const getBlogPosts = async () => {
+  const posts = await getAllBlogPosts();
+  const sortedPosts = sortPostsByPublishDesc(posts)
+
+  return sortedPosts
+    .map((p, idx): SortedBlogPost => ({
+      ...p,
+      previousPostSlug: idx < sortedPosts.length - 1 ? sortedPosts[idx + 1].slug : null,
+      nextPostSlug: idx > 0 ? sortedPosts[idx - 1].slug : null,
+    }));
 }
