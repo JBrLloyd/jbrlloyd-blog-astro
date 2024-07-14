af9c19f4e1e3918d7de683c58aa2780ffbbbeed9
Jackson Lloyd <jackson@jbrlloyd.dev>
2024-07-14 14:30:31 +1000

    add remark table of contents, remove quotes around quoteblock, using playform compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,56 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig, passthroughImageService } from "astro/config";
+import mdx from "@astrojs/mdx";
+import remarkCodeTitle from "remark-flexible-code-titles";
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
+import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import playformCompress from "@playform/compress";
 
-export default defineConfig({
-  site: 'https://blog.jbrlloyd.dev',
-  integrations: [ mdx(), sitemap(), solidJs({ devtools: true}), tailwind() ],
+/** @type { import("astro/config").AstroUserConfig } */
+const astroUserConfig = {
+  site: "https://blog.jbrlloyd.dev",
+  markdown: {
+    remarkPlugins: [
+      [
+        remarkToc,
+        {
+          heading: "contents",
+          maxDepth: 3,
+        },
+      ],
+      [
+        remarkCodeTitle,
+        {
+          titleTagName: "span",
+        },
+      ],
+    ],
+    rehypePlugins: [
+      rehypeAccessibleEmojis,
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          titleTagName: "CodeBlockTitle",
+          titleClassName: "custom-code-title",
+          titleProperties: (language, title) => ({
+            ["data-language"]: language,
+            title,
+          }),
+        },
+      ],
+    ],
+    shikiConfig: {},
+  },
+  integrations: [sitemap(), solidJs(), tailwind(), mdx(), playformCompress()],
   output: "hybrid",
   adapter: cloudflare({
-    imageService: passthroughImageService()
-  })
-});
+  }),
+};
+
+export default defineConfig(astroUserConfig);
